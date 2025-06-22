4fbd27ab8b15089821f1e4ac69ff70ba0c0de7ba
Tony <tony@pnholdings.com>
2025-06-22 10:43:26 +0700

    Initial Commit

---
+++ CNLTA-HP06-Final-PhotoBook/CNLTA_HP06_Final_PhotoBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CNLTA_HP06_Final_PhotoBookApp.swift
+//  CNLTA-HP06-Final-PhotoBook
+//
+//  Created by Tony Phan on 22/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CNLTA_HP06_Final_PhotoBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CNLTA-HP06-Final-PhotoBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CNLTA-HP06-Final-PhotoBook
+//
+//  Created by Tony Phan on 22/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
